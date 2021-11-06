4cd6a60d112bdc200e18776deaab1b63a9f7b23a
Dawid Dziedzic <dziedzicdev@gmail.com>
2021-11-06 20:13:00 +0100

    :sparkles: Add byte constants for keys

---
+++ server/binaryPayload/keys.go
@@ -0,0 +1,17 @@
+package binaryPayload
+
+const (
+	ArrowUp uint8 = iota << 1
+	ArrowDown
+	ArrowRight
+	ArrowLeft
+	ActionButton1
+	ActionButton2
+	ActionButton3
+	ActionButton4
+)
+
+const (
+	KeyUp uint8 = iota
+	KeyDown
+)
