75ebb71ec5140346918c84272aec99a153c8bc10
Patryk Marczak <m.patryk@outlook.com>
2021-11-07 17:54:28 +0100

    creating message added

--- interface/src/store/index.js
+++ interface/src/store/index.js
@@ -1,15 +1,23 @@
 import { createStore } from 'vuex'
+import { keyCodes, keyStates } from '../utils/keyCodes'
 
 export default createStore({
   state: {
   },
   mutations: {
     pressButton () {
-      console.log('button pressed')
     }
   },
   actions: {
-    pressButton ({ commit, state }, key) {
+    pressButton ({ commit, state }, { key, keyState }) {
+      const keyCode = keyCodes.get(key)
+      const kState = keyStates.get(keyState)
+      const keyStateMarker = parseInt(kState, 2)
+      const endOfStringOffset = 1
+
+      // zero is marking beggining of the string
+      const message = keyCode.slice(0, keyCode.length - endOfStringOffset) + keyStateMarker
+
       commit('pressButton')
     }
   },
---
+++ interface/src/utils/keyCodes.js
@@ -0,0 +1,15 @@
+export const keyCodes = new Map([
+  ['ARROW_UP', '00000000'],
+  ['ARROW_DOWN', '00000010'],
+  ['ARROW_RIGHT', '00000100'],
+  ['ARROW_LEFT', '00000000'],
+  ['ACTION_BUTTON_1', '00000110'],
+  ['ACTION_BUTTON_2', '00001000'],
+  ['ACTION_BUTTON_3', '00001100'],
+  ['ACTION_BUTTON_4', '00001110']
+])
+
+export const keyStates = new Map([
+  ['KEY_UP', '00000000'],
+  ['KEY_DOWN', '00000001']
+])
