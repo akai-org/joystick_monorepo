5ee1e9949fd8cc2ce9b702d97c22f6ac65337cdf
Patryk Marczak <m.patryk@outlook.com>
2021-11-07 22:17:36 +0100

    keyCodes changed to binary numbers

--- interface/src/store/index.js
+++ interface/src/store/index.js
@@ -19,11 +19,9 @@ export default createStore({
     pressButton ({ commit, state }, { key, keyState }) {
       const keyCode = keyCodes.get(key)
       const kState = keyStates.get(keyState)
-      const keyStateMarker = parseInt(kState, 2)
-      const endOfStringOffset = 1
 
       // zero is marking beggining of the string
-      const message = keyCode.slice(0, keyCode.length - endOfStringOffset) + keyStateMarker
+      const message = (keyCode | kState).toString(2)
 
       state.socket.send(message)
 
