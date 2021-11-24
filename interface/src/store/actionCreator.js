0f842fd260efe4e242772df76811c386d348e8f9
Patryk Marczak <m.patryk@outlook.com>
2021-11-24 20:21:08 +0100

    removed magic strings

--- interface/src/store/actionCreator.js
+++ interface/src/store/actionCreator.js
@@ -1,5 +1,6 @@
 import store from './index'
+import { PRESS_BUTTON } from './actions'
 
 export const dispatchPressButton = ({ key, keyState }) => {
-  store.dispatch('pressButton', { key, keyState })
+  store.dispatch(PRESS_BUTTON, { key, keyState })
 }
