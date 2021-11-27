c08a620d420aaea8022b1b8daa24bf394fe4c2e8
Patryk Marczak <m.patryk@outlook.com>
2021-11-27 11:46:20 +0100

    deleted even more comments

--- interface/src/interfaces/Keys.js
+++ interface/src/interfaces/Keys.js
@@ -1,15 +1,3 @@
-// export const ARROW_UP = 'ARROW_UP'
-// export const ARROW_DOWN = 'ARROW_DOWN'
-// export const ARROW_RIGHT = 'ARROW_RIGHT'
-// export const ARROW_LEFT = 'ARROW_LEFT'
-// export const ACTION_BUTTON_1 = 'ACTION_BUTTON_1'
-// export const ACTION_BUTTON_2 = 'ACTION_BUTTON_2'
-// export const ACTION_BUTTON_3 = 'ACTION_BUTTON_3'
-// export const ACTION_BUTTON_4 = 'ACTION_BUTTON_4'
-
-// export const KEY_UP = 'KEY_UP'
-// export const KEY_DOWN = 'KEY_DOWN'
-
 import { keyCodes, keyStates as kStates } from '../utils/keyCodes.js'
 
 export const keyActions = {}
