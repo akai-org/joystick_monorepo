9bc8c901903556535419cdcbf4e67c9485992449
Dawid Dziedzic <dziedzicdev@gmail.com>
2022-03-03 22:56:53 +0100

    :loud_sound: Add logger to server flow

--- server/logger/logger.go
+++ server/logger/logger.go
@@ -1,71 +1,61 @@
 package logger
 
 import (
-	"log"
-	"os"
-	"io/ioutil"
 	"io"
+	"io/ioutil"
+	"log"
 )
 
-
-const loggerFlags = log.Ldate | log.Ltime
+const loggerFlags = log.Ldate | log.Ltime | log.Lmsgprefix
 
 const (
-    DebugLevel uint8 = iota+1
-    InfoLevel
-    WarningLevel
-    ErrorLevel
-    FatalLevel
+	DebugLevel uint8 = iota + 1
+	InfoLevel
+	WarningLevel
+	ErrorLevel
+	FatalLevel
 )
 
-
-type Logger struct{
-	debugLogger log.Logger
-	infoLogger log.Logger
+type Logger struct {
+	debugLogger   log.Logger
+	infoLogger    log.Logger
 	warningLogger log.Logger
-	errorLogger log.Logger
-	fatalLogger log.Logger
+	errorLogger   log.Logger
+	fatalLogger   log.Logger
 }
 
-
-func New(level uint8) *Logger {
+func New(level uint8, writer io.Writer) *Logger {
 	return &Logger{
-        debugLogger:   *log.New(assignWriter(DebugLevel, level), "", loggerFlags),
-        infoLogger:    *log.New(assignWriter(InfoLevel, level), "", loggerFlags),
-        warningLogger: *log.New(assignWriter(WarningLevel, level), "", loggerFlags),
-        errorLogger:   *log.New(assignWriter(ErrorLevel, level), "", loggerFlags),
-        fatalLogger:   *log.New(assignWriter(FatalLevel, level), "", loggerFlags),
-    }
+		debugLogger:   *log.New(assignWriter(DebugLevel, level, writer), "DEBUG | ", loggerFlags),
+		infoLogger:    *log.New(assignWriter(InfoLevel, level, writer), "INFO  | ", loggerFlags),
+		warningLogger: *log.New(assignWriter(WarningLevel, level, writer), "WARN  | ", loggerFlags),
+		errorLogger:   *log.New(assignWriter(ErrorLevel, level, writer), "ERROR | ", loggerFlags),
+		fatalLogger:   *log.New(assignWriter(FatalLevel, level, writer), "PANIC | ", loggerFlags),
+	}
 }
 
-
-func assignWriter(level uint8, givenLevel uint8) io.Writer{
-	if givenLevel>level{
+func assignWriter(level uint8, givenLevel uint8, writer io.Writer) io.Writer {
+	if givenLevel > level {
 		return ioutil.Discard
 	}
-	return os.Stderr
+	return writer
 }
-
 
 func (logger *Logger) Debug(message string) {
 	logger.debugLogger.Print(message)
 }
 
-
 func (logger *Logger) Info(message string) {
 	logger.infoLogger.Print(message)
 }
-
 
 func (logger *Logger) Warning(message string) {
 	logger.warningLogger.Print(message)
 }
 
-
 func (logger *Logger) Error(message string) {
 	logger.errorLogger.Print(message)
 }
-
 
 func (logger *Logger) Fatal(message string) {
 	logger.fatalLogger.Print(message)
