f97e30784b365183dbe5a9476a0754a9ae695f27
nebulazc <67900555+nebulazc@users.noreply.github.com>
2022-03-06 16:35:26 +0100

    ✨ Cross arrows

--- interface/src/interfaces/controllers/cross-arrows/CrossArrows.vue
+++ interface/src/interfaces/controllers/cross-arrows/CrossArrows.vue
@@ -1,9 +1,59 @@
 <template>
-  <div>Cross Arrows</div>
+  <div>
+      <button id="top-btn" @touchstart="$emit(onTouchstart, keys.ARROW_UP)" @touchend="$emit(onTouchend, keys.ARROW_UP)">
+          <i class="fas fa-arrow-up"></i>
+      </button>
+      <button id="left-btn" @touchstart="$emit(onTouchstart, keys.ARROW_LEFT)" @touchend="$emit(onTouchend, keys.ARROW_LEFT)">
+          <i class="fas fa-arrow-left"></i>
+      </button>
+      <button id="right-btn" @touchstart="$emit(onTouchstart, keys.ARROW_RIGHT)" @touchend="$emit(onTouchend, keys.ARROW_RIGHT)">
+          <i class="fas fa-arrow-right"></i>
+      </button>
+      <button id="bottom-btn" @touchstart="$emit(onTouchstart, keys.ARROW_DOWN)" @touchend="$emit(onTouchend, keys.ARROW_DOWN)">
+          <i class="fas fa-arrow-down"></i>
+      </button>
+  </div>
+
+  <div id="rotate-warning">
+    <i class="fas fa-sync"></i>
+    <h1>Rotate your device.</h1>
+  </div>
+
+  <div class="fullscreen-btn" @click="tryToFullscreen()">
+    <i class="fas fa-expand-arrows-alt"></i>
+    Click to go fullscreen
+  </div>
 </template>
 
 <script>
+
+import { events } from '../../InterfacesEvents'
+import { keyActions as keys } from '../../Keys'
+
+// when phone is rotated to this orientation we lock it
+const wantedOrientation = 'landscape'
+
+screen.orientation.addEventListener('change', function (e) {
+  const orientation = e.target.type
+  if (orientation === `${wantedOrientation}-primary` || orientation === `${wantedOrientation}-secondary`) {
+    screen.orientation.lock(orientation)
+  }
+})
+
 export default {
-  name: 'CrossArrowsInterface'
+  name: 'CrossArrowsInterface',
+  emits: [events.onTouchstart, events.onTouchend],
+  data: function () {
+    return { ...events, keys }
+  },
+  methods: {
+    tryToFullscreen () {
+      document.body.requestFullscreen()
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  @import './CrossArrows.scss'
+</style>
